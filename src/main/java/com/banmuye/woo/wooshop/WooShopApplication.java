afc6bfa63bcd86f2f14653f9cd08cd242d4bc722
ZhouChunyang <2609714703@qq.com>
2022-05-19 20:55:05 +0800

    Initial commit

---
+++ src/main/java/com/banmuye/woo/wooshop/WooShopApplication.java
@@ -0,0 +1,13 @@
+package com.banmuye.woo.wooshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banmuye/woo/wooshop/WooShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banmuye.woo.wooshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
